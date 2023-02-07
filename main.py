d861588b942affc8188b6e6ad518ac3e480cbe0a
David <dbarron410@vt.edu>
2023-02-07 17:55:54 -0500

    main runs continuously now for easier useability

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ def main(persona, gender, language):
   # create an instance of pibot with optional parameters
   new_bot = PiBot(persona, gender, language)
 
-  # listen for user input
-  speech = new_bot.listen()
-  # process and produce a response to user input
-  response = new_bot.process(speech)
-  # verbalize the response
-  new_bot.verbalize(response)
+  # the bot will continuously listen for input, process it, and produce a response
+  # to exit the program say 'exit' or any similar command
+  while True:
+    # listen for user input
+    speech = new_bot.listen()
+    # process and produce a response to user input
+    response = new_bot.process(speech)
+    # verbalize the response
+    new_bot.verbalize(response)
 
   # or run all methods at once
   # new_bot.run()
@@ -27,7 +30,7 @@ if __name__ == '__main__':
   if personalization_choice.lower() == 'y' or personalization_choice.lower() == 'yes':
     persona = input("Enter the persona the bot will embody: ")
     gender = input("Enter the gender of the bot: ")
-    language = input("Enter the language the bot will speak: ")
+    language = input("Enter the language the bot will speak (see bot_properties.json to see available languages): ")
     main(persona, gender, language)
   else:
     main('bot', 'female', 'default')
