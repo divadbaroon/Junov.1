69202f996f5cc1b98b71ba5e13fa1d0b55ffc501
David <dbarron410@vt.edu>
2023-02-23 17:52:39 -0500

    Update documentation

--- speech_recognizer.py
+++ speech_recognizer.py
@@ -25,36 +25,36 @@ class SpeechRecognition:
         """
         print("\nListening...")  
         
-        # The current implementation of .recognize_once_async() can only attempt to listen for input for 5 seconds
+        # It appears the current implementation of .recognize_once_async() can only attempt to listen for input for 5 seconds
         # To accommodate for this limitation, recognition_attempt allows for 6 attempts at 5 seconds each, for a total of 30 seconds
         # This allows for a longer window of speech recognition while also avoiding repetitive prompts to the user
-        # However, this approach may not be ideal and a better solution is currently being sought
+        # This approach may not be ideal and a better solution is currently being sought
         recognition_attempt = 0
         
-        # starting timer
-        self.begin_timer = time()
+        # Time in seconds until the program ends if no speech is detected
+        # This is to prevent unintentional continuous listening to the user
+        time_until_exit = 300
         
-        # time in seconds until the program ends if no speech is detected
-        # this is to prevent unintentional continuous listening to the user
-        time_until_exit = 300  
+        # Starting timer
+        self.begin_timer = time()  
         
-        # continuously listen for speech
+        # Continuously listen for speech
         while True:
             try:
-                # a 5 second attempt to recognize the user's speech input
+                # A 5 second attempt to recognize the user's speech input
                 result = self.speech_recognizer.recognize_once_async().get() 
             except Exception as e:
                 print(f"Error occurred during speech recognition: {e}")
             
             recognition_attempt += 1
             
-            # if speech was recognized, print it and return the lowercase version
+            # If speech was recognized, print it and return the lowercase version
             if result.reason == speechsdk.ResultReason.RecognizedSpeech:
                 recognized_speech = result.text
                 print(f"\nInput:\nUser: {recognized_speech}")
                 return recognized_speech.lower().replace('.', '').strip()
             
-            # if the recognition was cancelled, check if it was due to an error
+            # If the recognition was cancelled, check if it was due to an error
             elif result.reason == speechsdk.ResultReason.Canceled:
                 cancellation_details = result.cancellation_details
                 print(f"Speech Recognition canceled: {cancellation_details.reason}")
@@ -63,16 +63,17 @@ class SpeechRecognition:
                     print(f"Error Detais: {cancellation_details.error_details}")
                     print("Did you set the speech resource key and region values?")
             
-            # if no speech was recognized after 30 seconds (6 attempts at 5 seconds each), notify the user
+            # If no speech was recognized after 30 seconds (6 attempts at 5 seconds each), notify the user that no speech was detected
             elif recognition_attempt == 6:
                 if result.reason == speechsdk.ResultReason.NoMatch:
                     print(f'No speech could be recognized: {result.no_match_details}')
                 recognition_attempt = 0
 
-            # ending timer
+            # Ending timer
             self.end_timer = time()
             elapsed_time = self.end_timer - self.begin_timer
             
+            # Terminate the program if no speech is detected for 5 minutes
             if elapsed_time >= time_until_exit:
                 print("The program has been terminated due to inactivity.")
                 sys.exit()
