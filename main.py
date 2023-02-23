6d13351f487fb29a475874b20d81afa71a0f501c
David <dbarron410@vt.edu>
2023-02-23 17:47:18 -0500

    clean up user input in main.py

--- main.py
+++ main.py
@@ -2,34 +2,35 @@
 Note:
 The following files must all be located within the same folder for the bot to function.
 < pibot.py, speech_recognizer.py, speech_processor.py, speech_verbalizer.py, sample_config.py,  
-  bot_properties.json, conversation_history.json >
+  bot_properties.py, bot_properties.json, conversation_history.json, startup_sound.wav(optional) >
 '''
+
 from pibot import PiBot
 
 def main(persona, gender, language):
   
-  # create an instance of pibot with optional parameters
+  # Create an instance of pibot with optional parameters
   new_bot = PiBot(persona, gender, language)
 
-  # the bot will continuously listen for input, process it, and produce a response
-  # exit the program by saying a generic exit command such as: 'exit', 'quit', 'terminate', or 'end conversation'
+  # The bot will continuously listen for input, process it, and produce a response
+  # Exit the program by saying a generic exit command such as: 'exit', 'quit', 'terminate', or 'end conversation'
   while True:
-    # listen for user input
+    # Listen for user input
     speech = new_bot.listen()
-    # process and produce a response to user input
+    # Process and produce a response to user input
     response = new_bot.process(speech)
-    # verbalize the response
+    # Verbalize the response
     new_bot.verbalize(response)
 
-    # or run all methods at once
-    # new_bot.run()
-
+    # Or run all methods at once
+    # new_bot.run()      
+    
 if __name__ == '__main__':
   personalization_choice = input('Would you like to personalize your bot? (Y/N): ')
-  if personalization_choice.lower() == 'y' or personalization_choice.lower() == 'yes':
-    persona = input("Enter the persona the bot will embody: ")
-    gender = input("Enter the gender of the bot: ")
-    language = input("Enter the language the bot will speak (see bot_properties.json to see available languages): ")
+  if personalization_choice.lower().strip() == 'y' or personalization_choice.lower().strip() == 'yes':
+    persona = input("Enter the persona the bot will embody: ").strip()
+    gender = input("Enter the gender of the bot: ").strip()
+    language = input("Enter the language the bot will speak (see bot_properties.json to see available languages): ").strip()
     main(persona, gender, language)
   else:
-    main('bot', 'female', 'default')
+    main(persona='Chatbot', gender='Female', language='English_USA')
