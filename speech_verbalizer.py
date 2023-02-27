66549cad3c5c607c009bb72a7eba800b16dfd1ae
David <dbarron410@vt.edu>
2023-02-27 10:33:32 -0500

    Update mute_status and persona in verbalize_speech method

--- speech_verbalizer.py
+++ speech_verbalizer.py
@@ -8,11 +8,9 @@ class SpeechVerbalizer:
 
 	Attributes:
 	bot_properties (BotProperties): A BotProperties object that contains information about the bot's properties.
-	persona (str): The persona of the bot retrieved from bot_properties.json
-    mute_status (bool): The mute status of the bot retrieved from bot_properties.json
-    speech_config (SpeechConfig): A configuration object that takes a subscription key and a region as arguments
-    audio_config (AudioOutputConfig): A configuration object that specifies the use of the default speaker
-    speech_synthesizer (SpeechSynthesizer): A synthesizer object that uses the above configurations to generate the spoken words
+	speech_config (SpeechConfig): A configuration object that takes a subscription key and a region as arguments
+	audio_config (AudioOutputConfig): A configuration object that specifies the use of the default speaker
+	speech_synthesizer (SpeechSynthesizer): A synthesizer object that uses the above configurations to generate the spoken words
 	"""
  
 	def __init__(self):
@@ -20,8 +18,6 @@ class SpeechVerbalizer:
 		Initializes a new SpeechVerbalizer object
 		"""
 		self.bot_properties = BotProperties()
-		self.persona = self.bot_properties.get_property('persona')
-		self.mute_status = self.bot_properties.get_property('mute_status')
 		self.speech_config = speechsdk.SpeechConfig(subscription = config.retrieve_secret('PiBot-API'), region = 'eastus')
 		self.audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 		self.speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=self.audio_config)
@@ -31,14 +27,18 @@ class SpeechVerbalizer:
 		Verbalizes a given string with a specified gender and langauge of voice.
 		:param speech (str): The speech to be verbalized.
 		"""	
-  
+
+		# Retrieve the bot's mute status and persona from bot_properties.json
+		mute_status = self.bot_properties.get_property('mute_status')
+		persona = self.bot_properties.get_property('persona')
+
 		# Check if there is speech to verbalize
 		if speech:
+      
 			# Check the bot's mute status
-			if not self.mute_status: 
-       
+			if not mute_status: 
+	   
 				# Retrieve the bot's voice name from bot_properties.json
-				# depending on the bot's specified gender and language.
 				voice_name = self.bot_properties.get_property('voice_name')
 
 				# Check if voice with given parameters exists
@@ -48,13 +48,17 @@ class SpeechVerbalizer:
 					self.speech_config.speech_synthesis_voice_name = 'en-US-JennyNeural' # used as a backup 
 
 				print('\nResponse:')
-				print(f'{self.persona.title()}: {speech}')
-    
-				# Verbalize the speech
+				print(f'{persona.title()}: {speech}')
+	
+				# Stop and clear any previous speech
+				self.speech_synthesizer.stop_speaking()
+				self.speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=self.audio_config)
+
+				# Verbalize the given speech
 				self.speech_synthesizer.speak_text(speech)
 			else:
 				print('\n(muted) Response:')
-				print(f'{self.persona.title()}: {speech}')
+				print(f'{persona.title()}: {speech}')
 		else:
 			print('No speech has been provided to verbalize.')
 	
