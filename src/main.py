b5ed62f152766a5f34592df8e8eb5c2e3b7cfeed
David <dbarron410@vt.edu>
2023-05-03 18:18:56 -0400

    Update secret names

--- src/main.py
+++ src/main.py
@@ -11,16 +11,19 @@ import configuration.config as config
 def main():
 	
 	# Retrieving the bot's secret values from Azure Key Vault
-	pibot_api = config.retrieve_secret('PiBot-API')
+	cognitive_services_api = config.retrieve_secret('Cognitive-Services-API')
 	luis_app_id = config.retrieve_secret('Luis-APP-ID')
 	luis_key = config.retrieve_secret('Luis-API')
 	openai_key = config.retrieve_secret('OpenAI-API')
 	weather_key = config.retrieve_secret('Weather-API')
 	news_key = config.retrieve_secret('News-API')
-	translator_key = config.retrieve_secret('PiBot-Translator-API')
+	translator_key = config.retrieve_secret('Translator-API')
+ 
+	# The region must match the region of the cognitive services resource
+	region = 'eastus'
 	
 	# Create an instance of pibot
-	new_bot = PiBot(pibot_api=pibot_api, luis_app_id=luis_app_id, luis_key=luis_key, openai_key=openai_key, weather_key=weather_key, translator_key=translator_key, news_key=news_key)
+	new_bot = PiBot(cognitive_services_api=cognitive_services_api, region=region, luis_app_id=luis_app_id, luis_key=luis_key, openai_key=openai_key, weather_key=weather_key, translator_key=translator_key, news_key=news_key)
 
 	# The bot will continuously listen for input, process it, and produce a response
 	# Exit the program by saying a generic exit command such as: 'exit', 'quit', 'terminate', or 'end conversation'
