fa61d59b99fb8b645f26a834953afe9a987861bc
David <dbarron410@vt.edu>
2023-05-12 11:40:20 -0400

    Add support for singular time metrics in StartTimer

--- src/pibot/bot_commands/timer.py
+++ src/pibot/bot_commands/timer.py
@@ -4,11 +4,11 @@ class StartTimer:
     def start_timer(self, user_time, metric):
         
         # Convert user_time to seconds
-        if metric == 'seconds':
+        if metric == 'seconds' or metric == 'second':
             pass
-        elif metric == 'minutes':
+        elif metric == 'minutes' or metric == 'minute':
             user_time *= 60
-        elif metric == 'hours':
+        elif metric == 'hours' or metric == 'hour':
             user_time *= 3600
 
         return {'start_timer': user_time, 'response': f'Ok, I will start a timer for {user_time} {metric[0]}'}
