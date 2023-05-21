3cfbae5efca9af0ea119b1aa41ae76dace17ec2c
David <dbarron410@vt.edu>
2023-05-21 15:47:33 -0400

    Now obtaining secrets in BotInitalizer class

--- main.py
+++ main.py
@@ -5,27 +5,12 @@ The following files must all be located within the same folder for the bot to fu
 bot_properties.py, bot_properties.json, conversation_history.json, startup_sound.wav(optional) >
 '''
 		
-from src.pibot.bot_initializer import PiBot
-import configuration.secrets.config as config
+from src.pibot import PiBot
 
 def main():
 	
-	# Retrieving the bot's secret values from Azure Key Vault
-	cognitive_services_api = config.retrieve_secret('Cognitive-Services-API')
-	clu_key = config.retrieve_secret('CLU-Key')
-	clu_endpoint = config.retrieve_secret('CLU-Endpoint')
-	clu_project_name = config.retrieve_secret('CLU-Poject-Name')
-	clu_deployment_name = config.retrieve_secret('CLU-Deployment-Name')
-	openai_key = config.retrieve_secret('OpenAI-API')
-	weather_key = config.retrieve_secret('Weather-API')
-	news_key = config.retrieve_secret('News-API')
-	translator_key = config.retrieve_secret('Translator-API')
- 
-	# The region must match the region of the cognitive services resource
-	region = 'eastus'
-	
 	# Create an instance of pibot
-	new_bot = PiBot(cognitive_services_api=cognitive_services_api, region=region, clu_endpoint=clu_endpoint, clu_project_name=clu_project_name, clu_deployment_name=clu_deployment_name, clu_key=clu_key, openai_key=openai_key, weather_key=weather_key, translator_key=translator_key, news_key=news_key)
+	new_bot = PiBot(persona='chatbot', gender='female', language='english')
 
 	# The bot will continuously listen for input, process it, and produce a response
 	# Exit the program by saying a generic exit command such as: 'exit', 'quit', 'terminate', or 'end conversation'
