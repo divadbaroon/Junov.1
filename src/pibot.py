60b3faa93bf548aeb941c5f06321b41c0aae4d9a
David <dbarron410@vt.edu>
2023-06-18 00:19:34 -0400

    change 'persona' to 'role'

--- src/pibot.py
+++ src/pibot.py
@@ -1,4 +1,4 @@
-from src.pibot_components.bot_initializer import BotInitializer
+from src.components.initializer import BotInitializer
 
 class PiBot:
 	"""PiBot is a class that provides a simple interface for creating a virtual assistant.
@@ -6,8 +6,8 @@ class PiBot:
  	for the speech recognition, speech processing, and speech verbalization.
  	As well as a run all method that performs all of the bot's functionalities."""
  
-	def __init__(self, persona='virtual assistant', gender='female', language='english'):
-		self.BotInitializer = BotInitializer(persona, gender, language)
+	def __init__(self, role='virtual assistant', gender='female', language='english'):
+		self.BotInitializer = BotInitializer(role, gender, language)
 		self.speech_recognition = self.BotInitializer.speech_recognition
 		self.speech_processor = self.BotInitializer.speech_processor
 		self.speech_verbalizer = self.BotInitializer.speech_verbalizer
@@ -25,7 +25,7 @@ class PiBot:
 		:param speech: (str) speech input
 		:return: (str) response to users speech
 		"""
-		return self.speech_processor.process_speech(speech)
+		return self.speech_processor.process_speech(speech, self.speech_verbalizer)
 	
 	def verbalize(self, response: str):
 		"""
@@ -34,7 +34,7 @@ class PiBot:
 		"""
 		self.speech_verbalizer.verbalize_speech(response)
 
-	def run(self):
+	def run_once(self):
 		"""
 		Peforms all of bot's functionalities including:
 		:.listen() # Listens for users speech
@@ -44,4 +44,16 @@ class PiBot:
 		speech = self.listen()
 		response = self.process(speech)
 		self.verbalize(response)
+
+	def run(self):
+		"""
+		Peforms all of bot's functionalities including:
+		:.listen() # Listens for users speech
+		:.process() # Processes and produces a response to users speech
+		:.verbalize() # Verbalizes the response
+		"""
+		while True:
+			speech = self.listen()
+			response = self.process(speech)
+			self.verbalize(response)
   
