95985ff2c1d4fbb1329e1df1424b6b1925aeb8a4
David <dbarron410@vt.edu>
2023-06-18 00:16:58 -0400

    Simplifying main

--- main.py
+++ main.py
@@ -2,20 +2,11 @@ from src.pibot import PiBot
 
 def main():
 	# Create an instance of pibot
-	new_bot = PiBot(persona='virtual assistant', gender='female', language='english')
+	new_bot = PiBot(role='virtual assistant', gender='female', language='english')
 
 	# The bot will continuously listen for user input, process it, and produce a response
 	# Exit the program by saying a generic exit command such as: 'exit', 'quit', 'terminate', or 'end conversation'
-	while True:
-		# Listen for user input
-		speech = new_bot.listen()
-		# Process and produce a response/action to user input
-		response = new_bot.process(speech)
-		# Verbalize the response
-		new_bot.verbalize(response)
-
-		# Or run all methods at once
-		# new_bot.run()      
-		
+	new_bot.run()
+         
 if __name__ == '__main__':
 	main()
