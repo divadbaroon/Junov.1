868a237741c467df47ca2f763c8638cc4967da84
David <dbarron410@vt.edu>
2023-07-14 22:39:19 -0400

    slight documentation change

--- src/components/settings/bot_settings/bot_settings_manager.py
+++ src/components/settings/bot_settings/bot_settings_manager.py
@@ -1,9 +1,8 @@
 import json
 import os
 
-# Get the current script's directory
+# Construct the path to the bot_settings.json file in the 'bot_settings' folder
 current_directory = os.path.dirname(os.path.abspath(__file__))
-# Construct the path to the bot_settings.json file in the 'voice' folder
 bot_settings_path = os.path.join(current_directory, 'bot_settings.json')
 
 class BotSettingsManager:
