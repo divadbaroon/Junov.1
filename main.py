ede453d63f12e7c8aaffaf1bd0d30cda37c976d9
David <dbarron410@vt.edu>
2023-07-21 14:41:50 -0400

    Simplify PiBot instantiation in main function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.pibot import PiBot
 
 def main():
-	# Create an instance of pibot
-	new_bot = PiBot(role='virtual assistant', gender='female', language='english')
+
+	new_bot = PiBot()
 
 	# The bot will continuously listen for user input, process it, and produce a response
 	# Exit the program by saying a generic exit command such as: 'exit', 'quit', 'terminate', or 'end conversation'
