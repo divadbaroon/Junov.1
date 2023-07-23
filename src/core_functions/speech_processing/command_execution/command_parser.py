9fae7067964051033f878f79459a71a0d7ba822a
David <dbarron410@vt.edu>
2023-07-23 13:14:28 -0400

    refactoring command parser and orchestrator classes

--- src/core_functions/speech_processing/command_execution/command_parser.py
+++ src/core_functions/speech_processing/command_execution/command_parser.py
@@ -1,77 +1,182 @@
+import os
+import yaml
+
+# Import all commands
+from src.components.commands.low_intent.ask_gpt.ask_gpt import AskGPT
 from src.components.commands.high_intent.translate_speech.translate_speech import TranslateSpeech
-from .command_orchestrator import CommandOrchestrator
+from src.components.commands.high_intent.get_weather.get_weather import GetWeather
+from src.components.commands.high_intent.web_searcher.web_searcher import WebSearcher
+from src.components.commands.high_intent.bot_behavior.bot_behavior import BotBehavior
+from src.components.commands.high_intent.set_timer.set_timer import StartTimer
+from src.components.commands.high_intent.generate_password.password_generator import PasswordGenerator
+from src.components.commands.high_intent.get_news.get_news import GetNews
+#from src.components.commands.high_intent.play_music.play_music import PlaySong
+from src.components.commands.high_intent.schedule_event.scheduler import Scheduler
+from src.components.conversation_history.conversation_history_manager import ConversationHistoryManager
 
 class CommandParser:
 	"""
-	A class that provides the most apporiate response and action to the user's speech given the similarity rankings.
-	This is done by retrieving the top intent  and its associated entity if applicable from the trained LUIS model.
-	If the top intent's score is less than 90% a response is instead created using OpenAI's GPT API.
-	If the top intent's score is greater than 90% the associated entity is retrieved and the appropriate action is executed.
-	"""
-  
-	def __init__(self, api_keys: dict, speech_verbalizer:object, intents_data:dict, bot_settings:object, voice_settings:object, command_settings:object):
-  
-		self.api_keys = api_keys
-	
-		# Initialize all bot commands
-		self.command = CommandOrchestrator(self.api_keys, speech_verbalizer, intents_data, bot_settings, voice_settings, command_settings)
-  
-		# load in the intents json produced by luis
-		self.intents_data = intents_data
-		
-		# Minimum intent score for a command to be exucuted
-		# If score is not met GPT-3.5-Turbo is used to create a response
-		self.MINIMUM_INTENT_SCORE = .90
-  
-		# loads in all currently supported bot commands 
-		self.commands = self.command.load_commands()
-
-		# Set to True if GPT was used to create a response
-		self.gpt_response = False
-
+ 	Initializes and parses all commands.
+  	"""
+ 
+	def __init__(self, api_keys:dict, speech_verbalizer:object, intents_data:dict, setting_objects:dict):
 		# retrieving the bot's role, language, and name
-		self.bot_settings = bot_settings
-		self.voice_settings = voice_settings
-		self.command_settings = command_settings
+		self.setting_objects = setting_objects
 		self._retrieve_bot_settings()
 
-	def parse_commands(self, speech:str) -> str:
-		"""
-		Provides the most apporiate response and action to the user's speech given the similarity rankings.
-		"""
+		self.intents_data = intents_data
+  
+		self.speech_verbalizer = speech_verbalizer
+  
+		# Initialize all bot commands
+		self._initilize_commands(api_keys)
+  
+	def _retrieve_bot_settings(self):
+		# retrieving the bot's role and language
+		bot_settings = self.setting_objects['bot_settings']
+		self.role = bot_settings.retrieve_property('role')
+		self.language = bot_settings.retrieve_property('language', 'current')
+		self.bot_name = bot_settings.retrieve_property('name')
+  
+	def _initilize_commands(self, api_keys:dict):
+		# Initialize all bot commands
+		self.request_gpt = AskGPT(api_keys['OPENAI-API-KEY'], self.setting_objects, self.bot_name)
+		self.request_translation = TranslateSpeech(api_keys['TRANSLATOR-API-KEY'], self.setting_objects)
+		self.request_weather = GetWeather(api_keys['WEATHER-API-KEY'])
+		self.browser_request  = WebSearcher()
+		self.bot_behavior = BotBehavior(self.speech_verbalizer, self.setting_objects)
+		self.timer = StartTimer(self.speech_verbalizer)
+		self.password_generator = PasswordGenerator()
+		self.conversation_history = ConversationHistoryManager()
+		self.request_news = GetNews(self.request_gpt, api_keys)
+		#self.request_song = PlaySong(self.command_setttings, api_keys)
+		self.schedule_event = Scheduler(self.setting_objects)
+  
+	def load_commands(self):
+		# path to 'supported_commands.yaml'
+		current_directory = os.path.dirname(os.path.abspath(__file__))
+		commands_file_path = os.path.join(current_directory, 'supported_commands.yaml')
+  
+		# loads all currently supported bot commands
+		with open(commands_file_path, 'r') as f:
+			commands = yaml.safe_load(f)
+			
+		return commands
 
-		# Extract top intent and top intent's score from intents_data
-		top_intent = self.intents_data["prediction"]["topIntent"]
-		top_intent_score = self.intents_data["prediction"]["intents"][top_intent]["score"]
-
-		# If the top intent's score is less than the minimum intent score, use GPT-3.5-Turbo to create a response
-		if top_intent_score < self.MINIMUM_INTENT_SCORE:
-			response = self.command.ask_gpt(speech)
-		else:
-			# Ensure the top intent is a supported command
-			if top_intent in self.commands:
-				response = getattr(self.command, top_intent.lower())()
-			else:
-				response = "Sorry, I don't understand that command. Please try asking again."
-
-		# If GPT-3 was not used, translate the response to the users specified language
-		# This is since GPT-3 is capable of translating the response itself
-		if not self.gpt_response and self.language != 'english' and top_intent != 'Translate_Speech':
-			response = TranslateSpeech(self.api_keys['TRANSLATOR-API-KEY'], self.bot_settings, self.voice_settings).translate_speech(response, 'english', self.language, True)
-   
+	def ask_gpt(self, speech:str):
+		response = self.request_gpt.ask_GPT(speech=speech) 
+		self.gpt_response = True
 		return response
 
-	@property
-	def intents_data(self):
-		return self._intents_data
+	def translate_speech(self):
+		speech_to_translate = self.intents_data["prediction"]["entities"]["translate_speech"][0]
+		current_language = self.intents_data["prediction"]["entities"]["language"][0]
+		new_language = self.language
+		response = self.request_translation.translate_speech(speech_to_translate, current_language, new_language, one_shot_translation=True)
+		return response
 
-	@intents_data.setter
-	def intents_data(self, value):
-		self._intents_data = value
-		self.command.intents_data = value
-	
-	def _retrieve_bot_settings(self) -> None:
-		"""Retrieves the bot's role, language, and name from the bot settings file"""
-		self.role = self.bot_settings.retrieve_property('role')
-		self.language = self.bot_settings.retrieve_property('language', 'current')
-		self.bot_name = self.bot_settings.retrieve_property('name')
+	def get_weather(self):
+		location = self.intents_data["prediction"]["entities"].get("weather_location", [0])[0]
+		response = self.request_weather.get_weather(location)
+		return response
+
+	def search_google(self):
+		search_request = self.intents_data["prediction"]["entities"]["search_google"][0]
+		response = self.browser_request.search_google(search_request)
+		return response
+
+	def open_website(self):
+		website = self.intents_data["prediction"]["entities"]["open_website"][0]
+		response = self.browser_request.open_website(website)
+		return response
+
+	def search_youtube(self):
+		search_request = self.intents_data["prediction"]["entities"]["search_youtube"][0]
+		response = self.browser_request.search_youtube(search_request)
+		return response
+
+	def change_role(self):
+		new_role = self.intents_data["prediction"]["entities"]["new_role"][0]
+		response = self.bot_behavior.change_role(new_role)
+		return response
+
+	def change_gender(self):
+		new_gender = self.intents_data["prediction"]["entities"]["new_gender"][0]
+		response = self.bot_behavior.change_gender(new_gender)
+		return response
+
+	def change_language(self):
+		new_language = self.intents_data["prediction"]["entities"]["new_language"][0]
+		response = self.bot_behavior.change_language(new_language)
+		return response
+
+	def change_voice(self):
+		response = self.bot_behavior.change_voice()
+		return response
+
+	def randomize_voice(self):
+		response = self.bot_behavior.randomize_voice()
+		return response
+
+	def start_timer(self):
+		user_time = self.intents_data["prediction"]["entities"]["user_timer"][0]
+		metric = self.intents_data["prediction"]["entities"]["metric"][0]
+		response = self.timer.start_timer(user_time, metric)
+		return response
+
+	def generate_password(self):
+		response = self.password_generator.generate_password()
+		return response
+
+	def mute(self):
+		response = self.bot_behavior.mute()
+		return response
+
+	def unmute(self):
+		response = self.bot_behavior.unmute()
+		return response
+
+	def pause(self):
+		response = self.bot_behavior.pause()
+		return response
+
+	def get_conversation_history(self):
+		response = self.conversation_history.get_conversation_history(self.role)
+		return response
+
+	def clear(self):
+		response = self.conversation_history.clear_conversation_history()
+		return response
+
+	def quit(self):
+		response = self.conversation_history.exit_and_clear_conversation_history()
+		self.setting_objects['bot_settings'].save_property('status', True, 'exit')
+		return response
+
+	def get_news(self):
+		response = self.request_news.get_news()
+		return response
+
+	def play_song(self):
+		song_name = self.intents_data["prediction"]["entities"]["song_name"][0]
+		response = self.request_song.play_song(song_name)
+		return response
+
+	def set_alarm(self):
+		hour = self.intents_data["prediction"]["entities"].get("hour", [0])[0]
+		minute = self.intents_data["prediction"]["entities"].get("minute", [0])[0]
+		second = self.intents_data["prediction"]["entities"].get("second", [0])[0]
+		am_or_pm = self.intents_data["prediction"]["entities"].get("am_or_pm", [0])[0]
+		response = self.schedule_event.set_alarm(hour, minute, second, am_or_pm)
+		return response
+
+	def set_reminder(self):
+		hour = self.intents_data["prediction"]["entities"].get("hour", [0])[0]
+		minute = self.intents_data["prediction"]["entities"].get("minute", [0])[0]
+		second = self.intents_data["prediction"]["entities"].get("second", [0])[0]
+		am_or_pm = self.intents_data["prediction"]["entities"].get("am_or_pm", [0])[0]
+		reminder = self.intents_data["prediction"]["entities"].get("reminder", [0])[0]
+		response = self.schedule_event.set_reminder(hour, minute, second, am_or_pm, reminder)
+		return response
+
+
