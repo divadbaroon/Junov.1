ad4b5b79ee0394c06ead28257f773e04e658a3b2
David <dbarron410@vt.edu>
2023-07-24 16:20:37 -0400

    added print statement

--- src/core_functions/speech_processing/speech_processor.py
+++ src/core_functions/speech_processing/speech_processor.py
@@ -20,6 +20,7 @@ class SpeechProcessor:
 		:param speech: (str) speech input
 		:return: (str) response to users speech and appropriate action to be taken
 		"""
+		print('\nThinking...')
 		# Returns a dictionary containing similarity rankings between the user's speech and the trained LUIS model
 		intents_data = self.get_intent.get_user_intent(speech)
 		# Updates the intents_data property in the command_orchestrator object with the intents data retrieved from the LUIS model
