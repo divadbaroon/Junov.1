42c29711664a7b58d8c659650487f481e436666e
David <dbarron410@vt.edu>
2023-07-24 16:18:53 -0400

    now use **kwargs for parameter handling

--- src/initialization/initializer.py
+++ src/initialization/initializer.py
@@ -13,7 +13,7 @@ class BotInitializer:
 	Initialize the bot's core functionalities: speech recognition, speech processing, and speech verbalization
 	'''
 	
-	def __init__(self, role:str, gender:str, language:str):
+	def __init__(self, **kwargs):
 		"""
 		Initializes PiBot with the following proccesses:
 		- Load in settings objects and save given params to bot settings
@@ -25,14 +25,14 @@ class BotInitializer:
 		self.setting_objects = self._load_in_setting_objects()
   
 		# save properties to bot settings
-		self._save_bot_properties(role, gender, language)
+		self._save_bot_properties(**kwargs)
   
 		# retrieve api keys as a dictionary
 		self.api_keys = ConfigurationManager().retrieve_api_keys()
   
 		# Initializing the bot's audio configuration, speech configuration, speech recognizer, and speech synthesizer
 		# The audio_config, speech_config, speech_recognizer, and speech_synthesizer are all being stored in a dictionary for ease of use  
-		self.speech_objects = self._setup_speech_and_audio(self.api_keys['COGNITIVE-SERVICES-API-KEY'], self.api_keys['REGION'], language)
+		self.speech_objects = self._setup_speech_and_audio(self.api_keys['COGNITIVE-SERVICES-API-KEY'], self.api_keys['REGION'], kwargs.get('language'))
 
 		# initializing the bot's core functionalities: speech recognition, speech processing, and speech verbalization
 		self._initialize_speech_functionalities()
@@ -50,19 +50,26 @@ class BotInitializer:
 		settings['command_settings'] = BotCommandManager()
 		return settings
 
-	def _save_bot_properties(self, role:str, gender:str, language:str) -> None:
+	def _save_bot_properties(self, **kwargs) -> None:
 		"""
   		Save the following bot properties to bot_settings.json
     	"""
 		# check if given params are valid before saving them
-		gender, language = self._check_gender_and_language(gender, language)
+		gender, language = self._check_gender_and_language(kwargs.get('gender'), kwargs.get('language'))
 		bot_settings = self.setting_objects['bot_settings']
-		if role:
-			bot_settings.save_property('role', role)
+		command_settings = self.setting_objects['command_settings']
+		if kwargs.get('name'):
+			bot_settings.save_property('name', kwargs.get('name'))
+		if kwargs.get('role'):
+			bot_settings.save_property('role', kwargs.get('role'))
 		if gender:
 			bot_settings.save_property('gender', gender, 'current')
 		if language:
 			bot_settings.save_property('language', language, 'current')
+		if kwargs.get('package'):
+			bot_settings.save_property('package', kwargs.get('package'))
+		if kwargs.get('prompt'):
+			command_settings.save_property('ask_gpt', 'prompt', kwargs.get('prompt'))
 
 	def _setup_speech_and_audio(self, cognitive_services_api:str, region:str, language:str) -> dict:
 		"""
