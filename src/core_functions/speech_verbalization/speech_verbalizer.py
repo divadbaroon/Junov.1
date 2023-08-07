34e07b5170b0faee7be917e9a902d2847fadf17f
David <dbarron410@vt.edu>
2023-08-07 07:21:26 -0400

    now passing profile name as an arg

--- src/core_functions/speech_verbalization/speech_verbalizer.py
+++ src/core_functions/speech_verbalization/speech_verbalizer.py
@@ -10,24 +10,27 @@ class SpeechVerbalizer:
 	A class that utilizes Azure's Cognitive Speech Service to verbalize the bot's response.
 	"""
  
-	def __init__(self, speech_objects:dict, api_keys:dict, setting_objects:dict):
+	def __init__(self, profile_name:str, speech_objects:dict, api_keys:dict, setting_objects:dict):
 		"""
 		Initializes a new SpeechVerbalizer object
 		"""
+		self.profile_name = profile_name
 		self.master_settings = setting_objects['master_settings']
 		self.profile_settings = setting_objects['profile_settings']
 		self.voice_settings = setting_objects['voice_settings']
 		# check which speech engine is used
 		if self.profile_settings.retrieve_property('voice_engine') == 'azure':
-			self.text_to_speech_engine = AzureTextToSpeech(speech_objects, setting_objects)
+			self.text_to_speech_engine = AzureTextToSpeech(self.profile_name, speech_objects, setting_objects)
 		else:
-			self.text_to_speech_engine = ElevenlabsTextToSpeech(api_keys, setting_objects)
+			self.text_to_speech_engine = ElevenlabsTextToSpeech(self.profile_name, api_keys, setting_objects)
    
 	def verbalize_speech(self, speech: str):
 		"""Verbalize the bot's response using the speech synthesizer."""
   
 		# loading in necessary data from 'master_settings.json'
 		self._load_in_settings()
+  
+		self.text_to_speech_engine.update_voice()
 
 		# initiale flag to check whether the speech synthesizer needs to be reconfigured or the bot is muted
 		if self._check_and_handle_preconditions(speech):
@@ -48,7 +51,7 @@ class SpeechVerbalizer:
   		Loading in necessary data from 'master_settings.json'
     	"""
 		self.master_settings.reload_settings()
-		self.bot_name = self.profile_settings.retrieve_property('name')
+		self.bot_name = self.profile_settings.retrieve_property('name', profile_name=self.profile_name)
 		self.mute_status = self.master_settings.retrieve_property('status', 'mute')
 		self.exit_status = self.master_settings.retrieve_property('status', 'exit')
 		self.reset_language = self.master_settings.retrieve_property('functions', 'reset_language')
