d4cd64abde498658a929a79ebe835293d8adba4e
David <dbarron410@vt.edu>
2023-08-07 07:24:05 -0400

    updated simulation

--- usage/simulations/time_traveler_and_alien_simulation.py
+++ usage/simulations/time_traveler_and_alien_simulation.py
@@ -6,24 +6,24 @@ def time_traveler_and_alien_simulation():
     
     The agents are created using Juno and use the following methods for its core functionality:
     -------------------------------------------------------------------------------------------------
-    .process() queries for and returns a response (Queries GPT with a given prompt and returns the response. Can also detect intent used for custom commands, however, that is not used in this example)                                    
+    .process() queries for and returns a response (Queries GPT with a given prompt and returns the response. Can also detect intent used for custom commands (stored in packages), however, that is not used in this example)                                    
     .verbalize() performs text-to-speech using Azure (very fast, good-quality) or ElevenLabs (slow, very high quality) voice engines 
     .listen() used to get user input via microphone (not used in this example)
     """
     
     # loading in profiles created using create_new_profile.py
-    time_traveler = Juno(profile='time_traveler')
-    alien = Juno(profile='alien')
+    time_traveler = Juno(profile_name='time_traveler')
+    alien = Juno(profile_name='alien')
     
-    # manually starting the conversation with a beginning input (for speech input use .listen())
-    beginning_input = 'I would like a cheese pizza with extra cheese please'
+    # manually starting the conversation with a beginning input 
+    beginning_input = 'Hello! Where are you from?'
     time_traveler.verbalize(beginning_input)
 
     # alien's initial response
     alien_response = alien.process(beginning_input)
     alien.verbalize(alien_response)
     
-    # endless loop of both agents exchanging dialog :) (for speech input use .listen())
+    # endless loop of both agents exchanging dialog 
     while True:
         
         # time_traveler response
