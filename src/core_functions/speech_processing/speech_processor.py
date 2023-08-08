c31aedc8eb60549fe1c72e017aa34aaf0e0c4488
David <dbarron410@vt.edu>
2023-08-08 07:28:33 -0400

    only get user intent if a package is being used

--- src/core_functions/speech_processing/speech_processor.py
+++ src/core_functions/speech_processing/speech_processor.py
@@ -1,5 +1,5 @@
 from .intent_recognition.intent_recognition import LuisIntentRecognition
-from importlib import import_module
+from .command_execution.command_orchestrator import CommandOrchestrator
  
 class SpeechProcessor:
 	"""
@@ -7,25 +7,26 @@ class SpeechProcessor:
 	"""
  
 	def __init__(self, api_keys: dict, setting_objects:dict, speech_verbalizer:object):
-		# dynamically import the CommandOrchestrator depending on the package specified in 'master_settings.json'
-		self.package_name = setting_objects['master_settings'].retrieve_property('package')
-		self.CommandOrchestrator = getattr(import_module(f"src.core_functions.speech_processing.command_execution.packages.{self.package_name}.command_orchestrator"), "CommandOrchestrator")
+		# retrieve the package name from profile settings object
+		self.package_name = setting_objects['profile_settings'].retrieve_property('package')
 		# initialize the LuisIntentRecognition and CommandOrchestrator objects
 		self.get_intent = LuisIntentRecognition(api_keys)
-		self.command_orchestrator = self.CommandOrchestrator(api_keys, speech_verbalizer, None, setting_objects)
+		self.command_orchestrator = CommandOrchestrator(api_keys, speech_verbalizer, None, setting_objects)
 
 	def process_speech(self, speech:str) -> str: 
 		"""
-		Processes the user's input using a trained LUIS model and produces an appropriate response and action.
+		Processes the user's input using a trained LUIS model (if a package is being used) and produces an appropriate response and action.
 		:param speech: (str) speech input
 		:return: (str) response to users speech and appropriate action to be taken
 		"""
 		print('\nThinking...')
+  
+		# If a package is being used, retrieve the user's intent using the trained LUIS model
 		if self.package_name: 
 			# Returns a dictionary containing similarity rankings between the user's speech and the trained LUIS model
 			intents_data = self.get_intent.get_user_intent(speech)
 			# Updates the intents_data property in the command_orchestrator object with the intents data retrieved from the LUIS model
 			self.command_orchestrator.intents_data = intents_data
-		# Provides the most apporiate response and action to the user's speech given the similarity rankings
-		response = self.command_orchestrator.process_command(speech)
-		return response
+   
+		# Provides the most apporiate response and action to the user's speech
+		return self.command_orchestrator.process_command(speech)
