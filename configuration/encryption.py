f5d2103341314e017e456c2fc9bf74b9226afc6c
David <dbarron410@vt.edu>
2023-09-03 02:43:17 -0400

    refactor name

--- configuration/encryption.py
+++ configuration/encryption.py
@@ -1,43 +1,21 @@
-from configuration.manage_secrets import DataHandler
-from configuration.manage_secrets import AzureKeyVaultHandler
-from configuration.manage_secrets import LocalSecretHandler
-from configuration.manage_secrets import AzureResourceManager
 from cryptography.fernet import Fernet
 import yaml
-import os
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-key_path = os.path.join(current_directory, 'secrets', 'key.key')
-
-class EncryptSecrets:
-	
-	def __init__(self):
-		self.data = DataHandler()._load_in_data()
-		self.preferred_secret_storage = self.data['preferred_secret_storage']
-		self.api_keys = self.data['api_keys']
-		self.api_keys = AzureResourceManager()._retrieve_azure_secrets(self.api_keys)
-  
-		self.azure_key_vault = AzureKeyVaultHandler()
-		self.encryption_handler = EncryptionHandler()
-		self.local_secrets = LocalSecretHandler(self.encryption_handler)
-  
-	def _setup(self):
-		self.azure_key_vault._save_keyvault_secrets(self.api_keys)
-		self.local_secrets._save_and_encrypt_local_secrets(self.api_keys)
+encryption_key_path = 'configuration/secrets/key.key'
   
 class EncryptionHandler:
   
 	def _save_key(self):
 		# Save the key
 		key = Fernet.generate_key()
-		with open(key_path, "wb") as key_file:
+		with open(encryption_key_path, "wb") as key_file:
 			key_file.write(key)
    
 		return key
    
 	def _retrieve_key(self):
 		# load the previously generated key
-		with open(key_path, "rb") as key_file:
+		with open(encryption_key_path, "rb") as key_file:
 			key = key_file.read()
 		return key
 	
@@ -82,5 +60,4 @@ class EncryptionHandler:
 		decrypted_api_keys = {key: cipher_suite.decrypt(value.encode()).decode() for key, value in encrypted_api_keys.items()}
 		return decrypted_api_keys
   
-if __name__ == '__main__':
-    EncryptSecrets()._setup()
+
