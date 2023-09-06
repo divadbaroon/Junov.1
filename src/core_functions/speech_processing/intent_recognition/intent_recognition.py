32a96619f9e2c46fb3d39125e4e397c64373f422
David <dbarron410@vt.edu>
2023-09-06 17:56:32 -0400

    now using CLU instead of LUIS

--- src/core_functions/speech_processing/intent_recognition/intent_recognition.py
+++ src/core_functions/speech_processing/intent_recognition/intent_recognition.py
@@ -1,12 +1,18 @@
-import requests
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.conversations import ConversationAnalysisClient
 
 class LuisIntentRecognition:
 		"""
 		A class that detects the user intent from the user's speech using the trained LUIS model.
 		"""
 
-		def __init__(self, api_keys: dict):
+		def __init__(self, api_keys: dict):	
 			self.api_keys = api_keys
+			self.clu_endpoint = self.api_keys['CLU-ENDPOINT']
+			self.clu_key = AzureKeyCredential(self.api_keys['CLU-API-KEY'])
+			self.clu_project_name = self.api_keys['CLU-PROJECT-NAME']
+			self.clu_deployment_name = self.api_keys['CLU-TRAINING-MODEL-NAME']   
+			self.client = ConversationAnalysisClient(self.clu_endpoint, self.clu_key)
 
 		def get_user_intent(self, speech:str) -> dict:
 			"""
@@ -14,21 +20,33 @@ class LuisIntentRecognition:
 			:param speech: (str) speech input
 			:return: (dict) Dictionary containing similarity rankings between the user's speech and the trained luis model
 			"""
-
+			
 			if isinstance(speech, dict):
 				speech = speech['translated_speech']
+    
+			query = speech
 
-			endpoint_url = (f"https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/{self.api_keys['LUIS-APPLICATION-ID']}"
-							f"/slots/production/predict?verbose=true&show-all-intents=true&log=true"
-							f"&subscription-key={self.api_keys['LUIS-API-KEY']}"
-							f"&query={speech}")
+			result = self.client.analyze_conversation(
+				task={
+					"kind": "Conversation",
+					"analysisInput": {
+						"conversationItem": {
+							"participantId": "1",
+							"id": "1",
+							"modality": "text",
+							"language": "en",
+							"text": query
+						},
+						"isLoggingEnabled": False
+					},
+					"parameters": {
+						"projectName": self.clu_project_name,
+						"deploymentName": self.clu_deployment_name,
+						"verbose": True
+					}
+				}
+			)
+		
+			return result
 
-			response = requests.get(endpoint_url)
-			# Check whether request was successful
-			if response.status_code == 200:
-				# Returned json file of the similarity rankings between the user's speech and the trained luis model
-				intents_data = response.json()
-			else:
-				raise ValueError(f"The request sent to the LUIS model was unsuccessful. Error: {response.status_code}")
 
-			return intents_data
