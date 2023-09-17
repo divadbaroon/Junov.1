1e0e9b93a48b699e9e365962a9b38527c722dedb
David <dbarron410@vt.edu>
2023-09-17 16:16:01 -0400

    refactor CLU class

--- training/begin_training_session.py
+++ training/begin_training_session.py
@@ -2,64 +2,55 @@ import json
 import os
 import requests
 import time
-
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.conversations.authoring import ConversationAuthoringClient
-
 from configuration.manage_secrets import ConfigurationManager
 
-# Relativing relative paths to training data
-project_header_path = os.path.join('training/assistant_training_data/project_header.json')
-utterance_file_path = os.path.join('training/assistant_training_data/utterances.json')
-patterns_file_path = os.path.join('training/assistant_training_data/patterns.json')
-entity_file_path = os.path.join('training/assistant_training_data/entities.json')
-intent_file_path = os.path.join('training/assistant_training_data/intents.json')
-
-class TrainLuisModel:
+class TrainCLUModel:
 	"""
- 	Creates and trains a fully functioning Azure LUIS model
-	Training data is imported and used to train the LUIS models intent recognition.
+ 	Creates and trains a fully functioning Azure CLU model
+	Training data is imported and used to train the CLU models intent recognition.
 	The model is mainly used for recognizing and responding to commands given by the user.
   	"""
 
-	def __init__(self) -> None:
-		"""Initializes the LUIS model"""
+	def __init__(self, folder_name) -> None:
+		"""Initializes the CLU model"""
   
 		self.configuration_manager = ConfigurationManager()
 		self.api_keys = self.configuration_manager.retrieve_api_keys()
+		self.project_name  = self.api_keys['CLU-PROJECT-NAME']
+		self.training_model_name = self.api_keys['CLU-TRAINING-MODEL-NAME']
 		self.endpoint = self.api_keys['CLU-ENDPOINT']
 		credential = AzureKeyCredential(self.api_keys['CLU-API-KEY'])
 		self.client = ConversationAuthoringClient(self.endpoint, credential)
+  
+		self.data_handler = DataHandler()
+		self.project_data = self.data_handler._prepare_training_data(folder_name)
 
-		self.project_name  = self.api_keys['CLU-PROJECT-NAME']
-		self.project_data = self._load_in_project_data()
-  
-		self.training_model_name = self.api_keys['CLU-TRAINING-MODEL-NAME']
-  
 	def import_project_data(self) -> None:
 		"""
 		Creates a new CLU project and imports the training data located within 
-  		the /training sub-directory into it
+		the /training sub-directory into it
 		"""
 		poller = self.client.begin_import_project(
 				project_name=self.project_name,
 				project={
-					"assets": self.project_data,
-					"stringIndexType": "Utf16CodeUnit",
-					"metadata": {
-						"projectKind": "Conversation",
-						"settings": {"confidenceThreshold": 0.7},
-						"projectName": self.project_name,
-						"multilingual": True,
-						"description": "Trying out CLU",
-						"language": "en-us",
-					},
-					"projectFileVersion": "2022-05-01",
+				"assets": self.project_data,
+				"stringIndexType": "Utf16CodeUnit",
+				"metadata": {
+					"projectKind": "Conversation",
+					"settings": {"confidenceThreshold": 0.7},
+					"projectName": self.project_name,
+					"multilingual": True,
+					"description": "Trying out CLU",
+					"language": "en-us",
 				},
+				"projectFileVersion": "2022-05-01",
+			},
 		)
 		print(f"\nImporting training data to project: {self.project_name}")
 		print(f'\n{poller.result()}')
-
+   
 	def train_conversation_model(self):
 		"""
 		Begins a training session for the newly created CLU model
@@ -88,7 +79,7 @@ class TrainLuisModel:
 		if response.status_code == 202:
 			operation_location = response.headers["operation-location"]
 			print(f"\nTraining started. Poll the operation location to get status: {operation_location}")
-			self.wait_for_operation_to_complete(operation_location, headers)
+			self._wait_for_operation_to_complete(operation_location, headers)
 		else:
 			print(f"Training failed. Error: {response.json()}")
    
@@ -114,44 +105,11 @@ class TrainLuisModel:
 		if response.status_code == 202:
 			operation_location = response.headers["operation-location"]
 			print(f"\nDeployment started. Poll the operation location to get status: {operation_location}")
-			self.wait_for_operation_to_complete(operation_location, headers)
+			self._wait_for_operation_to_complete(operation_location, headers)
 		else:
 			print(f"Deployment failed. Error: {response.json()}")
-
-	def _load_in_project_data(self, project) -> dict:
-     
-		# Load in project_header information
-		with open(project_header_path) as f:
-			project_header = json.load(f) 
-
-		# Load in example utterances 
-		with open(utterance_file_path) as f:
-			utterance_data = json.load(f) 
-	
-		# Load in example patterns
-		with open(patterns_file_path) as f:
-			pattern_data = json.load(f)
-
-		# Load in entities
-		with open(entity_file_path) as f:
-			entity_data = json.load(f) 
-
-		# Load in intents
-		with open(intent_file_path) as f:
-			intent_data = json.load(f) 
-
-		# Initialize an empty 'assets' dictionary within project_data
-		assets = {}
-
-		# Update 'assets' with the various types of data
-		assets["intents"] = intent_data["intents"]  
-		assets["entities"] = entity_data
-		assets["utterances"] = utterance_data 
-
-		# Save the project data to a file
-		return assets
    
-	def wait_for_operation_to_complete(self, operation_location, headers):
+	def _wait_for_operation_to_complete(self, operation_location, headers):
 		while True:
 			time.sleep(5)  # Wait for 5 seconds before checking the status again
 			response = requests.get(operation_location, headers=headers)
@@ -163,19 +121,59 @@ class TrainLuisModel:
 				print("Operation failed.")
 				break
    
+class DataHandler:
+
+	def _prepare_training_data(self, folder_name) -> dict:
+
+		# Initialize assets
+		assets = {
+			"projectKind": "Conversation",
+			"intents": [],
+			"entities": [],
+			"utterances": []
+		}
+
+		# Load in the training data
+		# if project == 'assistant_training_data', load in the basic training data as well
+		if folder_name == 'assistant_training_data':
+			assets = self._load_in_project_data(folder_name, assets)
+			assets = self._load_in_project_data('basic_training_data', assets)
+		else:
+			assets = self._load_in_project_data(folder_name, assets)
+
+		return assets
+
+	def _load_in_project_data(self, project, assets) -> dict:
+		"""
+		Loads in the project data from the project_header.json file
+		"""
+		# Walk through the directory tree
+		for dirpath, dirnames, filenames in os.walk(f'training/{project}'):
+			for filename in filenames:
+				if filename.endswith('.json'):
+					full_path = os.path.join(dirpath, filename)
+					
+					# Load the JSON data
+					file_data = self._load_in_file(full_path)
+
+					 # Merge the loaded data into assets based on the file type
+					if filename == 'intents.json':
+						assets["intents"].extend(file_data["intents"])
+					elif filename == 'entities.json':
+						assets["entities"].extend(file_data)  
+					elif filename == 'utterances.json':
+						assets["utterances"].extend(file_data)
+		return assets
+
+	def _load_in_file(self, file_path) -> dict:
+		with open(file_path, 'r') as f:
+			return json.load(f)
+
 if __name__ == "__main__":
-	"""
-	Runs the training session
-	"""
-	print("\nImporting training data from the /training sub-directory into a new CLU project for training and deployment.")
-	print("Please note: The entire process may take up to 5 minutes to complete.")
-
- 
-	test = TrainLuisModel()
-	test.import_project_data()
-	test.train_conversation_model()
-	test.deploy_conversation_model()
-
-
-  
 	
+	folder_name = input("Enter the name of the folder used for training (i.e 'assistant_training_data', 'basic_training_data'): ")
+	
+	new_model = TrainCLUModel(folder_name)
+	new_model.import_project_data()
+	new_model.train_conversation_model()
+	new_model.deploy_conversation_model()
