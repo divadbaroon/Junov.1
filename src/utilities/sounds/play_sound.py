d74da4936deca8881af001b19b44a07cfd0a196f
David <dbarron410@vt.edu>
2023-09-17 16:13:03 -0400

    add try except block

--- src/utilities/sounds/play_sound.py
+++ src/utilities/sounds/play_sound.py
@@ -6,10 +6,13 @@ def play_bot_sound(sound_name):
 	
 	# Get path to wav file 
 	current_directory = os.path.dirname(os.path.abspath(__file__))
-	sound_path = os.path.join(current_directory, 'sounds', f'{sound_name}.wav')
+	sound_path = os.path.join(current_directory, 'sound_names', f'{sound_name}.wav')
  
 	# Play sound if it exists
 	if os.path.isfile(sound_path):
-		playsound(sound_path)
+		try:
+			playsound(sound_path)
+		except:
+			print(f"Error playing {sound_name}.wav. Ensure {sound_name}.wav is a valid wav file")
 	else:
 		print(f"{sound_name}.wav not found. Ensure {sound_name}.wav is located within the 'sounds' directory")
