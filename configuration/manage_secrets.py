cd084661a566f4ad7d77165754a738c4223ebdb0
David <dbarron410@vt.edu>
2023-09-17 17:11:08 -0400

    refactor ConfigurationManager

--- configuration/manage_secrets.py
+++ configuration/manage_secrets.py
@@ -1,17 +1,15 @@
-from .utils import AzureKeyVaultHandler, EnvironmentVariableHandler, LocalSecretHandler, DataHandler, AzureResourceManager
-from .encryption import EncryptionHandler
+from .utils.secret_retrieval_handler import SecretRetrieval
+from .utils.encryption_handler import EncryptionHandler
 
 class ConfigurationManager:
 	"""
 	used to manage and retrieve secrets from 'secret_config.yaml'
 	"""
 	def __init__(self):
-		self.azure_key_vault = AzureKeyVaultHandler()
-		self.environment_variables = EnvironmentVariableHandler()
 		self.encryption_handler = EncryptionHandler()
-		self.local_secrets = LocalSecretHandler(self.encryption_handler)
+		self.secret_manager = SecretRetrieval(self.encryption_handler)
   
-		self.data = DataHandler()._load_in_data()
+		self.data = self.secret_manager._load_in_data()
 		self.preferred_secret_storage = self.data['preferred_secret_storage']
 		self.api_keys = self.data['api_keys']
   
@@ -21,9 +19,9 @@ class ConfigurationManager:
 		infrastrucutre has been created in Azure. The secrets values are initially stored in an Azure keyvault.
 		and encrypted and stored locally for ease of use.
     	"""
-		self.api_keys = AzureResourceManager()._retrieve_azure_secrets(self.api_keys)
-		self.azure_key_vault._save_keyvault_secrets(self.api_keys)
-		self.local_secrets._save_and_encrypt_local_secrets(self.api_keys)
+		self.api_keys = self.secret_manager._retrieve_azure_secrets(self.api_keys)
+		self.secret_manager._get_keyvault_secrets(self.api_keys)
+		self.secret_manager._save_and_encrypt_local_secrets(self.api_keys)
   
 	def retrieve_api_keys(self) -> dict:
 		"""
@@ -31,11 +29,11 @@ class ConfigurationManager:
 		secret values are stored in a hash map for ease of use
    		"""
 		if self.preferred_secret_storage == 'azure':
-			api_keys = self.azure_key_vault._get_keyvault_secrets(self.api_keys)
+			api_keys = self.secret_manager._get_keyvault_secrets(self.api_keys)
 		elif self.preferred_secret_storage == 'environment':
-			api_keys = self.environment_variables._get_environment_secrets(self.api_keys)
+			api_keys = self.secret_manager._get_environment_secrets(self.api_keys)
 		elif self.preferred_secret_storage == 'local':
-			api_keys = self.local_secrets._load_in_local_secrets()
+			api_keys = self.secret_manager._load_in_local_secrets()
 		return api_keys 
 
 	def retrieve_config_value(self, value) -> dict:
