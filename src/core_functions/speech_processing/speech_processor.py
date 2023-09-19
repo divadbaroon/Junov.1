e91181f7b75b1d6ea04db299de0f37f2e8c97abb
David <dbarron410@vt.edu>
2023-09-19 01:09:33 -0400

    Refactor speech processor class

--- src/core_functions/speech_processing/speech_processor.py
+++ src/core_functions/speech_processing/speech_processor.py
@@ -7,8 +7,11 @@ class SpeechProcessor:
 	"""
  
 	def __init__(self, api_keys: dict, setting_objects:dict, speech_verbalizer:object):
-		self.package_name = setting_objects['profile_settings'].retrieve_property('package')
-		self.get_intent = LuisIntentRecognition(api_keys)
+		self.profile_settings = setting_objects['profile_settings']
+		self.voice_settings = setting_objects['voice_settings']
+		self.package_name = self.profile_settings.retrieve_property('package')
+		self.package_name = self.profile_settings.retrieve_property('package')
+		self.get_intent = LuisIntentRecognition(api_keys, self.profile_settings, self.voice_settings)
 		self.command_orchestrator = CommandOrchestrator(api_keys, speech_verbalizer, None, setting_objects)
 
 	def process_speech(self, speech:str) -> str: 
@@ -19,12 +22,16 @@ class SpeechProcessor:
 		"""
   
 		print('\nThinking...')
+  
 		# If a package is provided, retrieve the user's intent using the trained LUIS model
 		if self.package_name: 
 			# Returns a dictionary containing similarity rankings between the user's speech and the trained LUIS model
 			intents_data = self.get_intent.get_user_intent(speech)
 			# Updates the intents_data property in the command_orchestrator object with the intents data retrieved from the LUIS model
 			self.command_orchestrator.intents_data = intents_data
-   
-		# Provides the most apporiate response and action to the user's speech
-		return self.command_orchestrator.process_command(speech)
+			# Provides the most apporiate response and action to the user's speech
+			return self.command_orchestrator.process_command(speech)
+		else:
+			# asks gpt
+			return  self.command_orchestrator.ask_gpt(speech)
+
