e49a31c83a5bb370621e9b4dbc87ab4001742be6
David <dbarron410@vt.edu>
2023-09-19 01:22:45 -0400

    refactor intent recognition class

--- src/core_functions/speech_processing/intent_recognition/intent_recognition.py
+++ src/core_functions/speech_processing/intent_recognition/intent_recognition.py
@@ -1,43 +1,37 @@
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.conversations import ConversationAnalysisClient
 
-class LuisIntentRecognition:
+class CLUIntentRecognition:
 		"""
-		A class that detects the user intent from the user's speech using the trained LUIS model.
+		A class that detects the user intent from the user's speech using the trained CLU model.
 		"""
 
 		def __init__(self, api_keys: dict, profile_settings:object, voice_settings:object):	
-			self.api_keys = api_keys
-			self.clu_endpoint = self.api_keys['CLU-ENDPOINT']
-			self.clu_key = AzureKeyCredential(self.api_keys['CLU-API-KEY'])
-			self.clu_project_name = self.api_keys['CLU-PROJECT-NAME']
-			self.clu_deployment_name = self.api_keys['CLU-TRAINING-MODEL-NAME']   
+			# Load in secrets needed to use the CLU model
+			self._load_in_secrets(api_keys)	 
+			# Initialize CLU client
 			self.client = ConversationAnalysisClient(self.clu_endpoint, self.clu_key)
+			# load in profile and voice settings
 			self.profile_settings = profile_settings
 			self.voice_settings = voice_settings
 
 		def get_user_intent(self, speech:str) -> dict:
 			"""
-			Retrieves the similarity rankings between the user's speech and the trained LUIS model.
+			Retrieves the similarity rankings between the user's speech and the trained CLU model.
 			:param speech: (str) speech input
-			:return: (dict) Dictionary containing similarity rankings between the user's speech and the trained luis model
+			:return: (dict) Dictionary containing similarity rankings between the user's speech and the trained CLU model
 			"""
-			
-			language  = self.profile_settings.retrieve_property('language')
-			language_code = self.voice_settings.retrieve_language_code(language)
-    
-			query = speech
 
-			result = self.client.analyze_conversation(
-				task={
+			# Create job to send to CLU model
+			job = {
 					"kind": "Conversation",
 					"analysisInput": {
 						"conversationItem": {
 							"participantId": "1",
 							"id": "1",
 							"modality": "text",
-							"language": language_code,
-							"text": query
+							"language": self._get_current_language(),
+							"text": speech
 						},
 						"isLoggingEnabled": False
 					},
@@ -47,8 +41,25 @@ class LuisIntentRecognition:
 						"verbose": True
 					}
 				}
-			)
+
+			# Similarity rankings between the user's speech and the trained CLU model
+			return self.client.analyze_conversation(task=job)
+
+		def _load_in_secrets(self, api_keys:dict) -> None:
+			"""
+			Initializes secrets needed to use the CLU model.
+			"""
+			self.api_keys = api_keys
+			self.clu_endpoint = self.api_keys['CLU-ENDPOINT']
+			self.clu_key = AzureKeyCredential(self.api_keys['CLU-API-KEY'])
+			self.clu_project_name = self.api_keys['CLU-PROJECT-NAME']
+			self.clu_deployment_name = self.api_keys['CLU-TRAINING-MODEL-NAME']
    
-			return result
+		def _get_current_language(self) -> str:
+			"""
+			Retrieves the current language being used by the bot.
+			"""
+			language  = self.profile_settings.retrieve_property('language')
+			return self.voice_settings.retrieve_language_code(language)
 
 
