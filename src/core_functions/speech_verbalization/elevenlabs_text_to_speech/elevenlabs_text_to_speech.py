852cdf22201092ea53aead0933808014ed161ee6
David <dbarron410@vt.edu>
2023-10-02 23:08:13 -0400

    refactor ElevenlabsTextToSpeech

--- src/core_functions/speech_verbalization/elevenlabs_text_to_speech/elevenlabs_text_to_speech.py
+++ src/core_functions/speech_verbalization/elevenlabs_text_to_speech/elevenlabs_text_to_speech.py
@@ -4,14 +4,16 @@ class ElevenlabsTextToSpeech:
   """
   A class that utilizes Elevenlabs' API to verbalize the bot's response.
   """
-  def __init__(self, profile_name:str, api_keys:dict, setting_objects:dict):
+  def __init__(self, profile_name:str, api_keys:dict, setting_objects:object):
     self.profile_name = profile_name
     set_api_key(api_keys['ELEVENLABS-API-KEY'])
     self.profile_settings = setting_objects['profile_settings']
-    self.voice_name = 'Adam'
+    self.voice_name = self.profile_settings.retrieve_property('voice_name', profile_name=self.profile_name)
   
-  def text_to_speech(self, speech:str):
-    """Peforms text-to-speech using Elevenlabs' API."""
+  def text_to_speech(self, speech:str, language_country_code:str):
+    """
+    Peforms text-to-speech using Elevenlabs' API.
+    """
     audio = generate(
       text=speech,
       voice=self.voice_name,
@@ -21,5 +23,7 @@ class ElevenlabsTextToSpeech:
     play(audio)
     
   def update_voice(self):
-    """Updates the voice name used for Elevenlabs' API."""
+    """
+    Updates the voice name used for Elevenlabs' API.
+    """
     self.voice_name =  self.profile_settings.retrieve_property('voice_name', profile_name=self.profile_name)
