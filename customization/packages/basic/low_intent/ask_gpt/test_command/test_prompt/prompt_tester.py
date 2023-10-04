90764899cb4969fb14b8b656c9fb103825e5e188
David <dbarron410@vt.edu>
2023-10-04 17:26:02 -0400

    refactor respository

---
+++ customization/packages/basic/low_intent/ask_gpt/test_command/test_prompt/prompt_tester.py
@@ -0,0 +1,33 @@
+import json
+import os
+from configuration.secrets import key_vault
+from src.utilities.settings.master_settings.master_settings_manager import MasterSettingsManager
+from customization.packages.basic.low_intent.ask_gpt.ask_gpt import AskGPT
+
+# Relative path to mock_prompt.json file
+mock_prompt_path = os.path.join('src/components/commands/ask_gpt/test_command/test_prompt/mock_prompt.json')
+
+class TestPrompt:
+    """A class for testing prompts on the AskGPT command"""
+    
+    def __init__(self):
+        self.openai_key = key_vault.retrieve_secret('OpenAI-API')
+        self.master_settings = MasterSettingsManager()
+        self.bot_name = self.master_settings.retrieve_property('bot_name')
+        
+        #self.prompt = self._read_prompt()
+        self.prompt = f"You are a helpful virtual assistant named Sarah. Keep your responses concise yet informative to the user."
+        self.ask_gpt = AskGPT(self.openai_key, self.master_settings, self.bot_name, self.prompt)
+        
+    def _ask_GPT(self):
+        response = self.ask_gpt.ask_GPT(speech='What are your thoughts on climate change?', model='gpt-4', manual_request=True)
+        return response
+    
+    def _read_prompt(self):
+        with open(mock_prompt_path, 'r') as file:
+            data = json.load(file)
+        return data["prompt"]
+            
+if __name__ == '__main__':
+    test_prompt = TestPrompt()
+    print(test_prompt._ask_GPT())
---
+++ customization/packages/virtual_assistant/high_intent/translate_speech/test_command/test_translate_speech.py
@@ -0,0 +1,23 @@
+import unittest
+from configuration.secrets.key_vault import KeyVaultManager
+from customization.packages.virtual_assistant.high_intent.translate_speech.translate_speech import TranslateSpeech
+
+class TestTranslateSpeech(unittest.TestCase):
+    """Class for testing the TranslateSpeech command"""
+    
+    def setUp(self):
+        self.key_vault = KeyVaultManager()
+        self.translator_key = self.key_vault.retrieve_secret('TRANSLATOR-API-KEY')
+        self.translator = TranslateSpeech(self.translator_key)
+        self.translator_incorrect = TranslateSpeech("wrong key")
+        
+    def test_translator(self):
+        response = self.translator.translate_speech("Hello", "English", "Spanish", one_shot_translation=True)
+        self.assertEqual(response, "Hola")
+
+    def test_translator_with_incorrect_key(self):
+        response = self.translator_incorrect.translate_speech("Hello", "English", "Spanish", one_shot_translation=True)
+        self.assertEqual(response, 'Sorry, there was an error while trying to translate: Hello. Try asking again.')
+        
+if __name__ == '__main__':
+    unittest.main()
