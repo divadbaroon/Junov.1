bf621c1265991cfb5b912eb58cec35f11b70602f
David <dbarron410@vt.edu>
2023-10-18 18:08:19 -0400

    refactor performance logger

--- src/utilities/logs/log_performance.py
+++ src/utilities/logs/log_performance.py
@@ -1,59 +1,84 @@
 import yaml
-import datetime
-import time
+import time 
 from src.utilities.settings.master_settings.master_settings_manager import MasterSettingsManager
+from src.customization.profiles.profile_manager import ProfileManager
 
 class PerformanceLogger:
 	"""
-	A class that logs the performance of a function and stores the data in 'logs.yaml'
+	Logs the performance of each method in a session
 	"""
-
+ 
 	def __init__(self):
 		self.profile_name = MasterSettingsManager().retrieve_property('profile')
-		self.log_path = f'src/profiles/profile_storage/{self.profile_name}/logs.yaml'
-		#self._initialize_new_session()
-		self.todays_date = datetime.datetime.now().strftime("%Y-%m-%d")
-		
+		self.entity_name = ProfileManager().retrieve_property('name', self.profile_name)
+		self.log_path = f'src/customization/profiles/profile_storage/{self.profile_name}/logs.yaml'
+		self.new_session = True
+  
 	def log_operation(self, func):
 		"""
-		decorator that logs the performance of a function and stores the data in 'logs.yaml'
+		Decorator that logs the performance of each method in a session
 		"""
 		def wrapper(*args, **kwargs):
+			"""
+			Wrapper function that logs the performance of each method in a session
+			"""
+   
+			# get name of function
 			action = func.__name__
-			
-			if action == 'listen':
-				input_data = "User input"
-			elif action in ['process_speech', 'verbalize_speech']:
-				input_data = args[1]
-			elif action == '_retrieve_top_intent':
-				input_data = args
-				if input_data is None:
-					input_data = "ask_gpt"
-			else:
-				input_data = f"{args} {kwargs}"
+   
+			# Determine input_data based on the action (function name)
+			action = self._make_method_name_readable(action, args, kwargs)
 
-			log_template = self._load_fresh_log_template(action)
-
-			log_template[action]["Input"] = input_data
-
-			start_time = time.perf_counter()
-			result = func(*args, **kwargs)
-			end_time = time.perf_counter()
-
-			log_template[action]["Run time"] = end_time - start_time
-
-			if result: 
-				log_template[action]["Success"] = True
-				log_template[action]["Output"] = result
-			else:
-				log_template[action]["Success"] = False
-				log_template[action]["Output"] = 'error'
-				log_template[action]["Error"] = result
-
-			self._save_data(log_template, action)
+			# Log the operation
+			result = self._log_operation(action, func, *args, **kwargs)
 			return result
 		return wrapper
+   
+	def _make_method_name_readable(self, action: str, args, kwargs) -> str:
+		"""
+		Converts a method name to a human-readable string
+		"""
+		# Determine input_data based on the action (function name)
+		if action == 'listen':
+			action = "User input"
+		elif action in ['process_speech', 'verbalize_speech']:
+			action = args[1]
+		elif action == '_retrieve_top_intent':
+			action = args
+			if action is None:
+				action = "ask_gpt"
+		else:
+			action = f"{args} {kwargs}"
+   
+	def _log_operation(self, action: str, func, *args, **kwargs):
+		"""
+		Logs the operation
+		"""
+		# Load the log template
+		log_data = self._load_fresh_log_template(action)
+  
+		# Time the operation
+		start_time = time.perf_counter()
+		# Get the result of the function
+		result = func(*args, **kwargs)
+		end_time = time.perf_counter()
 
+		# Save runtime
+		log_data[action]["Run time"] = end_time - start_time
+
+		# Save input_data
+		if result: 
+			log_data[action]["Success"] = True
+			log_data[action]["Output"] = result
+		else:
+			log_data[action]["Success"] = False
+			log_data[action]["Output"] = 'error'
+			log_data[action]["Error"] = result
+
+		self._save_data(log_data)
+  
+		return result
+   
 	def _load_fresh_log_template(self, action):
 		return {
 			action: {
@@ -64,35 +89,27 @@ class PerformanceLogger:
 				"Run time": None
 			}
 		}
-
-	def _save_data(self, log_entry, action):
-		data = self._load_in_data()
-		current_time = datetime.datetime.now().strftime("%H:%M:%S")
-		
-		if self.todays_date not in data:
-			data[self.todays_date] = {
-				"Time": current_time,
-				"Logs": []
-			}
-		else:
-			data[self.todays_date]['Time'] = current_time  # Update time for every log
-		
-		log_entry_data = {
-			"Action": action,
-			**log_entry[action]  # This will unpack the inner dictionary
-		}
-		data[self.todays_date]['Logs'].append(log_entry_data)
-
-		with open(self.log_path, "w") as f:
-			yaml.safe_dump(data, f)
-
-	def _load_in_data(self):
+  
+	def _save_data(self, log_data):
+		"""
+		Saves the log operation
+		"""
+		existing_data = self._Load_in_data()
+		log_data.update(existing_data)
 		try:
-			with open(self.log_path, "r") as f:
-				loaded_data = yaml.safe_load(f)
-				if loaded_data is None:  # If the file is empty, it will return None
-					return {"log_sessions": []}
-				return loaded_data
-		except (FileNotFoundError, yaml.YAMLError):
-			print('The file: "logs.yaml" was not found or is improperly formatted.')
-			return {"log_sessions": []}
+			with open(self.log_path, "w", encoding="utf-8") as f:
+				yaml.safe_dump(log_data, f)
+		except FileNotFoundError:
+			print('The file "logs.yaml" is missing. Make sure all files are located within the same folder.')
+		 
+	def _Load_in_data(self):
+		"""
+		Loads the log data
+		"""
+		try:
+			with open(self.log_path, 'r', encoding='utf-8') as f:
+				data = yaml.safe_load(f) or {}
+		except FileNotFoundError:
+			print('The file "logs.yaml" is missing.\nMake sure all files are located within the same folder.')
+			data = {}
+		return data
