aaffcde8f3519a2f613f8a7bdea404307c2fb92b
David <dbarron410@vt.edu>
2023-10-23 22:44:43 -0400

    refactor custom profile

--- usage/create_profile/create_new_profile.py
+++ usage/create_profile/create_new_profile.py
@@ -1,22 +1,24 @@
 import yaml
 from src.customization.profiles.profile_manager import ProfileManager
  
-def create_custom_profile():
+def create_custom_profile(config=None, profile_name='test'):
     """
     Create and save a custom profile to be used by Juno
     """
     
-    # open file located within /profiles/profile_storage/defualt/settings.yaml
-    with open ('usage/create_profile/example_profile.yaml', 'r') as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
+    if not config:
+        # open file located within /profiles/profile_storage/defualt/settings.yaml
+        with open ('usage/create_profile/example_profile.yaml', 'r') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
         
     # Creates and saves profiles
-    ProfileManager().create_profile(config=config, profile_name='obama') 
+    ProfileManager().create_profile(config=config, profile_name=profile_name) 
     
     # To see profiles 
     #print(ProfileManager().load_profile_data())
     
     # delete profiles
-    # ProfileManager().remove_profile(profile_name='time_traveler') 
-    
-create_custom_profile()
+    #ProfileManager().remove_profile(profile_name='a') 
+
+if __name__ == '__main__':
+    create_custom_profile(profile_name='a')
